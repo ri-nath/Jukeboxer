d48e08e45a328e2a4bb5214a797d5d69cf0264e2
Rishi Nath <rishi.nath@gmail.com>
2019-08-16 17:50:45 -0700

    basic mod

---
+++ src/main/java/jukebox/Jukebox.java
@@ -0,0 +1,24 @@
+package jukebox;
+
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+
+import java.lang.reflect.Field;
+
+@Mod(modid = Jukebox.MODID, version = Jukebox.VERSION)
+public class Jukebox {
+    static final String MODID = "jukebox";
+    static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        try {
+            Field ticker = Minecraft.getMinecraft().getClass().getDeclaredField("mcMusicTicker");
+            ticker.setAccessible(true);
+            ticker.set(Minecraft.getMinecraft(), new JukeboxTicker(Minecraft.getMinecraft()));
+        } catch (Exception error) {
+            error.printStackTrace();
+        }
+    }
+}
